7aeb34503d80bf2e08ad49ee48797a0432dbdaae
lin <never199410@gmail.com>
2017-08-27 21:08:49 +0800

    fix

--- spliting-rabbitMQ-tracting-logs.sh
+++ spliting-rabbitMQ-tracting-logs.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 Number=1
 current_date=$(date +%Y%m%d)
 past_date=$(date -d "$current_date - $Number days" +%Y%m%d)
@@ -9,5 +10,5 @@ CURL='/usr/bin/curl'
 DELETE_CURLARGS="-i -u username:password -XDELETE"
 CREATE_CURLARGS="-i -u username:password -H \"content-type:application/json\" -XPUT"
 CREATE_PARAMMET="-d'{\"format\":\"text\",\"pattern\":\"publish.#\"}'"
-curl -i -u open.api:open.api -H "content-type:application/json" -XPUT http://localhost:15672/api/traces/%2fopen/$current_name  -d'{"format":"text","pattern":"publish.#"}'
+curl -i -u username:password -H "content-type:application/json" -XPUT http://localhost:15672/api/traces/%2fopen/$current_name  -d'{"format":"text","pattern":"publish.#"}'
 delete_raw="$($CURL $DELETE_CURLARGS $delete_url)"
